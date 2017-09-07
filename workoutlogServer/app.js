adad39ee30026a717c11877d0227c4ea5d237e36
Christian Ingram <clingram1@aol.com>
2017-09-07 10:19:24 -0400

    jwtsecretstuff

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express=require('express');
 var app=express();
 var bodyParser=require('body-parser');
@@ -9,8 +10,9 @@ User.sync(); // sync( {force: true}) WARNING: This will DROP the table!
 
 app.use(bodyParser.json());
 app.use(require('./middleware/headers'));
-app.use('/api/user', require('./routes/user'));
-app.use('/api/login', require('./routes/session')); //login route
+app.use(require('./middleware/validate-session'));
+app.use('/api/user', require('./routes/user')); //creating a user
+app.use('/api/login', require('./routes/session')); //logging in a user
 app.use('/api/test', function(req, res){
 	res.send("Hello World");
 });
