fc4c615fd05157e79d0595e574b136c972c33b9a
Christian Ingram <clingram1@aol.com>
2017-09-08 11:01:21 -0400

    definition route

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -5,14 +5,15 @@ var bodyParser=require('body-parser');
 var sequelize=require('./db.js');
 
 var User=sequelize.import(__dirname + '\\models\\user');
-User.sync();  
-// User.sync({force: true}); //WARNING: This will DROP the table!
+//User.sync();  
+sequelize.sync() // User.sync({force: true}); //WARNING: This will DROP the table!
 
 app.use(bodyParser.json());
 app.use(require('./middleware/headers'));
 app.use(require('./middleware/validate-session'));
 app.use('/api/user', require('./routes/user')); //creating a user
 app.use('/api/login', require('./routes/session')); //logging in a user
+app.use('/api/definition', require('./routes/definition'));
 app.use('/api/test', function(req, res){
 	res.send("Hello World");
 });
